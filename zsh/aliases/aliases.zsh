772c6486821998f63f2b1eb447c3c8559ad1981f
Koen Mevissen <mevissen.koen@gmail.com>
2022-03-17 21:43:39 +0100

    disable aliases

--- zsh/aliases/aliases.zsh
+++ zsh/aliases/aliases.zsh
@@ -1,23 +1,23 @@
-alias production="gcloud config configurations activate production && gcloud container clusters get-credentials production-europe-west1-c --zone=europe-west1-c"
+# alias production="gcloud config configurations activate production && gcloud container clusters get-credentials production-europe-west1-c --zone=europe-west1-c"
 
-alias production-regional="gcloud config configurations activate production && gcloud container clusters get-credentials production-europe-west1 --zone=europe-west1"
+# alias production-regional="gcloud config configurations activate production && gcloud container clusters get-credentials production-europe-west1 --zone=europe-west1"
 
-alias staging="gcloud config configurations activate staging && gcloud container clusters get-credentials staging-europe-west1-c --zone=europe-west1-c"
+# alias staging="gcloud config configurations activate staging && gcloud container clusters get-credentials staging-europe-west1-c --zone=europe-west1-c"
 
-alias development="gcloud config configurations activate development && gcloud container clusters get-credentials development-europe-west1-c --zone=europe-west1-c"
+# alias development="gcloud config configurations activate development && gcloud container clusters get-credentials development-europe-west1-c --zone=europe-west1-c"
 
-alias bi-test="gcloud config configurations activate bi-test && gcloud container clusters get-credentials travix-bi-test --zone=europe-west1-d"
+# alias bi-test="gcloud config configurations activate bi-test && gcloud container clusters get-credentials travix-bi-test --zone=europe-west1-d"
 
-alias bi-production="gcloud config configurations activate bi-production"
+# alias bi-production="gcloud config configurations activate bi-production"
 
-alias tooling="gcloud config configurations activate tooling && gcloud container clusters get-credentials tooling --zone=europe-west1-c"
+# alias tooling="gcloud config configurations activate tooling && gcloud container clusters get-credentials tooling --zone=europe-west1-c"
 
-alias dev-pricing="gcloud config configurations activate dev-pricing && gcloud container clusters get-credentials trvx-dev-pricing --zone=europe-west1-c"
+# alias dev-pricing="gcloud config configurations activate dev-pricing && gcloud container clusters get-credentials trvx-dev-pricing --zone=europe-west1-c"
 
-alias stg-common="gcloud config configurations activate stg-common && gcloud container clusters get-credentials stg-common --zone=europe-west1"
+# alias stg-common="gcloud config configurations activate stg-common && gcloud container clusters get-credentials stg-common --zone=europe-west1"
 
-alias prd-common="gcloud config configurations activate prd-common && gcloud container clusters get-credentials prd-common --zone=europe-west1"
+# alias prd-common="gcloud config configurations activate prd-common && gcloud container clusters get-credentials prd-common --zone=europe-west1"
 
-alias dev-datascience="gcloud config configurations activate dev-datascience && gcloud container clusters get-credentials dev-datascience --zone=europe-west1"
+# alias dev-datascience="gcloud config configurations activate dev-datascience && gcloud container clusters get-credentials dev-datascience --zone=europe-west1"
 
-alias prd-datalake-governance="gcloud config configurations activate prd-datalake-governance"
+# alias prd-datalake-governance="gcloud config configurations activate prd-datalake-governance"
